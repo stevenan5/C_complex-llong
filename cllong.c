d58d3d0b7a8cd33607b12bfcf9aa549f4bb91457
Steven An <steventeddy555@gmail.com>
2017-01-24 10:11:45 -0800

    add definition and fix function prototype/arguments

---
+++ cllong.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include "cllong.h"
+
+int main(){
+
+	//add test functions
+
+}
+
+void cll_set(cllong* op, long long real, long long imag){
+
+	op->real = real;
+	op->imag = imag;
+
+}
+
+void cll_get(cllong* rop, cllong op){
+
+	rop->real = op.real;
+	rop->imag = op.imag;
+
+}
+
+void cll_add(cllong* rop, cllong op1, cllong op2){
+
+	rop->real = op1.real + op2.real;
+	rop->imag = op1.imag + op2.imag;
+}
+
+void cll_sub(cllong* rop, cllong op1, cllong op2){
+
+	rop->real = op1.real - op2.real;
+	rop->imag = op1.imag - op2.imag;
+}
+
+void cll_mul(cllong* rop, cllong op1, cllong op2){
+
+	rop->real = op1.real * op2.real - op1.imag * op2.imag;
+	rop->imag = op1.real * op2.imag + op1.imag + op2.real;
+}
+
+void cll_conjugate(cllong* rop, cllong op){
+
+	rop->real = op1.real;
+	rop->imag = -op2.imag;
+
+}
+
+void cll_norm2(cllong* rop, cllong op){
+	
+	cllong tmp;
+	cll_conjugate(&tmp, op);
+	cll_multiply(rop, op, tmp);
+
+}
+
+long long cll_real(cllong op){
+
+	return op.real;
+
+}
+
+long long cll_imag(cllong op){
+
+	return op.imag;
+
+}
+
+void cll_print(cllong op){
+
+	printf("real: %lld complex: %lld\n", op.real, op.imag);
+
+}
--- cllong.h
+++ cllong.h
@@ -9,17 +9,18 @@ typedef struct{
 
 }cllong;
 
-void cll_set(cllong* op);
-void cll_get(cllong* rop, cllong* op);
+void cll_set(cllong* op, long long real, long long imag);
+void cll_get(cllong* rop, cllong op);
 
-void cll_add(cllong* rop, cllong op1, cllong op1);
-void cll_sub(cllong* rop, cllong op1, cllong op1);
-void cll_mul(cllong* rop, cllong op1, cllong op1);
+void cll_add(cllong* rop, cllong op1, cllong op2);
+void cll_sub(cllong* rop, cllong op1, cllong op2);
+void cll_mul(cllong* rop, cllong op1, cllong op2);
 void cll_conjugate(cllong* rop, cllong op);
 void cll_norm2(cllong* rop, cllong op);
 
 long long cll_real(cllong op);
 long long cll_imag(cllong op);
 
+void cll_print(cllong op);
 
 #endif
