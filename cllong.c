824b26698c10d5c03dfa2e6724526da2c7cc2ccc
Steven An <steventeddy555@gmail.com>
2017-01-26 09:39:05 -0800

    finish testing

--- cllong.c
+++ cllong.c
@@ -3,7 +3,37 @@
 
 int main(){
 
-	//add test functions
+	cllong t1, t2, t3, res;
+
+	cll_set(&t1, 3LL, 4LL);
+	printf("set real/imag\n");
+	cll_print(t1);
+
+	cll_get(&t2, t1);
+	printf("use get function\n");
+	cll_print(t2);
+	
+	cll_add(&t3, t1, t2);
+	printf("use add\n");
+	cll_print(t3);
+
+	cll_sub(&res, t1, t3);
+	printf("use sub\n");
+	cll_print(res);
+
+	cll_mul(&res, t3, t1);
+	printf("use mul\n");
+	cll_print(res);
+	
+	cll_conjugate(&res, t3);
+	printf("use conjugate\n");
+	cll_print(res);
+
+	cll_norm2(&res, t1);
+	printf("use norm2\n");
+	cll_print(res);
+
+	printf("%lld, %lld \n", cll_real(res), cll_imag(res));
 
 }
 
@@ -36,21 +66,19 @@ void cll_sub(cllong* rop, cllong op1, cllong op2){
 void cll_mul(cllong* rop, cllong op1, cllong op2){
 
 	rop->real = op1.real * op2.real - op1.imag * op2.imag;
-	rop->imag = op1.real * op2.imag + op1.imag + op2.real;
+	rop->imag = op1.real * op2.imag + op1.imag * op2.real;
 }
 
 void cll_conjugate(cllong* rop, cllong op){
 
-	rop->real = op1.real;
-	rop->imag = -op2.imag;
+	rop->real = op.real;
+	rop->imag = -op.imag;
 
 }
 
 void cll_norm2(cllong* rop, cllong op){
 	
-	cllong tmp;
-	cll_conjugate(&tmp, op);
-	cll_multiply(rop, op, tmp);
+	cll_set(rop, cll_real(op) * cll_real(op), cll_imag(op) * cll_imag(op));
 
 }
 
